f962adf7acc7b25cc9d8e37d978b066c9580309a
gang zhang <gang.zhang@pps.io>
2019-09-24 21:52:12 -0400

    initial test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Anim from './components/graphworker';
+class RunWorker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { rotation: 0 };
+    this.tick = this.tick.bind(this);
+  }
+
+  componentDidMount() {
+    requestAnimationFrame(this.tick);
+  }
+
+  tick() {
+    const rotation = this.state.rotation + 0.04;
+    this.setState({ rotation });
+    requestAnimationFrame(this.tick);
+  }
+
+  render() {
+    return <Anim rotation={this.state.rotation} width={200} height={200} />;
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <RunWorker/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
